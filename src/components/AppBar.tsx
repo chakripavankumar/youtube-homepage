6868ae285766b6323969e04c95dd3a7c3e92a9a0
“[chakripavankumar]” <[siriyalapavankumar@gmail.com]>
2024-02-06 22:52:22 +0530

    video

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -2,8 +2,16 @@
  
   export const AppBar=()=>{
 return (
-    <div>
-        
+    <div className=" flex  justify-between">
+<div>
+    Youtube
+</div>
+<div>
+    search bar
+</div>
+<div>
+    sign in
+</div>
     </div>
 )
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { VideoGrid } from "@/components/VideoGrid";
+import { AppBar } from "@/components/AppBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
+        <AppBar></AppBar>
        <VideoGrid></VideoGrid>
+     
     </div>
   );
 }
